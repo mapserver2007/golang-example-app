b435943798e890c526093a8eb79eac1762894da4
tanaka_ryuichi <mapserver2007@gmail.com>
2020-08-03 11:57:38 +0900

    Responseエラーをカスタマイズ

--- grpc-web/common/http/custom_http_error.go
+++ grpc-web/common/http/custom_http_error.go
@@ -6,34 +6,25 @@ import (
 	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"github.com/labstack/gommon/log"
+	log "github.com/mapserver2007/golang-example-app/grpc-web/common/log"
 	"google.golang.org/grpc/status"
 )
 
 type errorBody struct {
-	Message string `json:"error,omitempty"`
+	Message string `json:"message"`
+	Status  int    `json:"status"`
 }
 
 func CustomHTTPError(ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler,
 	w http.ResponseWriter, _ *http.Request, err error) {
-	const fallback = `{"error": "failed to marshal error message11"}`
+	statusCode := runtime.HTTPStatusFromCode(status.Code(err))
 	w.Header().Set("Content-type", marshaler.ContentType())
-	w.WriteHeader(runtime.HTTPStatusFromCode(status.Code(err)))
+	w.WriteHeader(statusCode)
 
-	jsonErr := json.NewEncoder(w).Encode(errorBody{
-		Message: err.Error(),
-	})
-
-	if jsonErr != nil {
-		s, _ := w.Write([]byte(fallback))
-		log.Info(s)
+	if json.NewEncoder(w).Encode(errorBody{
+		Message: status.Convert(err).Message(),
+		Status:  statusCode,
+	}) != nil {
+		log.Fatal("Response error")
 	}
-
-	// s, _ := w.Write([]byte(fallback))
-
-	// aaa, ok := errors.Cause(err).(grpcError)
-
-	// log.Info(s)
-	// log.Error(aaa)
-	// log.Info(ok)
 }
